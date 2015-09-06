04268f89aaa5fa6bd4535f8b86d092ae85395548
Saku Hakamaa <sakuukuli@hotmail.com>
2015-09-06 12:29:36 +0300

    Initial commit

--- RimWorld_SimplifyPathTranslations.py
+++ RimWorld_SimplifyPathTranslations.py
@@ -8,9 +8,9 @@ __author__ = 'Sakuukuli'
 def printhelp():
     """ Print information about the script.
     """
-    print("RimWorld Translation Comparison Script")
-    print("Compares two translation directories and finds differences in tags.")
-    print("Usage: RimWorld_CompareTranslations.py <Directory 1> <Directory 2>")
+    print("RimWorld Path Translation Simplification Script")
+    print("Simplifies multi-line path translations to the new simpler syntax.")
+    print("Usage: RimWorld_SimplifyPathTranslations.py <Directory>")
 
 
 def printhelperror():
@@ -21,51 +21,30 @@ def printhelperror():
     print("Enclose folder names in double quotes.")
 
 
-def list_tags(translationdir):
-    """
+def format_path(path):
+    path = path.replace('[', '.')
+    path = path.replace(']', '')
+    return path
 
-    :param translationdir:
-    :return:
-    """
-    templist = []
-    for dirpath, dirnames, filenames in os.walk(translationdir):
 
-        if os.path.basename(dirpath) == 'Keyed':
-            # Go through all the files one by one
-            for filename in [f for f in filenames if f.endswith('.xml')]:
-
-                # Parse the .xml file with ElementTree
-                deffile = ETree.parse(os.path.join(dirpath, filename))
-                defroot = deffile.getroot()
-
-                for child in defroot:
-                    templist.append((child.tag, os.path.join('Keyed', filename)))
-
-        if os.path.basename(os.path.split(dirpath)[0]) == 'DefInjected':
-            # Go through all the files one by one
-            for filename in [f for f in filenames if f.endswith('.xml')]:
-
-                # Parse the .xml file with ElementTree
-                deffile = ETree.parse(os.path.join(dirpath, filename))
-                defroot = deffile.getroot()
-
-                for child in defroot:
-                    templist.append((child.tag, os.path.join('DefInjected', os.path.basename(dirpath), filename)))
-
-    return templist
+def replace_escapechars(text):
+    escapechars = {'&quot;': '"', '&amp;': '&', '&lt;': '<', '&gt;': '>'}
+    for i, j in escapechars.items():
+        text = text.replace(i, j)
+    return text
 
 
 # Save the arguments
 arguments = sys.argv[1:]
 # Save the directories in variables
-if len(arguments) == 2:
-    firstDirPath = arguments[0]
-    secondDirPath = arguments[1]
+if len(arguments) == 1:
+    translationDirPath = arguments[0]
 # If incorrect number of arguments then print help
 elif not arguments:
     printhelp()
     sys.exit(2)
 else:
+    printhelp()
     printhelperror()
     sys.exit(2)
 
@@ -73,38 +52,56 @@ else:
 print("--------------------------------------------------------------------")
 print("RimWorld Translation Comparison Script")
 print("")
-print("Directory 1 is \"" + firstDirPath + "\"")
-print("Directory 2 is \"" + secondDirPath + "\"")
+print("Directory is \"" + translationDirPath + "\"")
 print("--------------------------------------------------------------------")
 print("")
 
+translationDirPath = os.path.join(translationDirPath, 'DefInjected')
+
 # Check if the entered RimWorld installation folder was correct
-if not os.path.exists(firstDirPath):
-    print("Directory 1 is invalid.")
-    sys.exit(2)
-elif not os.path.exists(secondDirPath):
-    print("Directory 2 is invalid.")
+if not os.path.exists(translationDirPath):
+    print("Directory is invalid.")
     sys.exit(2)
 
 # Go through all the folders one by one
 # dirpath is the full path to the current def directory, dirnames is a list of directories in the current directory
 # and filenames is a list of files
 
-firstList = list_tags(firstDirPath)
-secondList = list_tags(secondDirPath)
+for dirpath, dirnames, filenames in os.walk(translationDirPath):
 
-firstUnique = []
-secondUnique = []
+    for filename in [f for f in filenames if f.endswith('.xml')]:
 
-for element in firstList:
-    if element not in secondList:
-        firstUnique.append(element)
+        # Parse the .xml file with ElementTree
+        defFile = ETree.parse(os.path.join(dirpath, filename))
+        defRoot = defFile.getroot()
 
-for element in secondList:
-    if element not in firstList:
-        secondUnique.append(element)
+        tempfilename = filename + '.temp'
 
+        if defRoot.find('rep'):
+            for index, repElement in enumerate(defRoot):
+                if repElement.tag == 'rep':
+                    path = repElement.find('path').text
+                    trans = repElement.find('trans').text
+                    whitespace = repElement.tail
 
-print(firstUnique)
-print(secondUnique)
-print("")
+                    path = format_path(path)
+
+                    defRoot.remove(repElement)
+
+                    defRoot.insert(index, ETree.Element(path))
+                    defRoot[index].text = trans
+                    defRoot[index].tail = whitespace
+
+            defFile.write(os.path.join(dirpath, tempfilename), encoding="utf-8", xml_declaration=False, method="html")
+
+            defFile = open(os.path.join(dirpath, filename), 'w+')
+            processedFile = open(os.path.join(dirpath, tempfilename), 'r')
+            processedFileText = replace_escapechars(processedFile.read())
+            defFile.write('<?xml version="1.0" encoding="utf-8" ?>\n')
+            defFile.write(processedFileText)
+            defFile.write('\n')
+
+            defFile.close()
+            processedFile.close()
+
+            os.remove(os.path.join(dirpath, tempfilename))
