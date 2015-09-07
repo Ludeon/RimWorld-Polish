0a5af45a1868c6098c0cafad37753a68f22517bd
Saku Hakamaa <sakuukuli@hotmail.com>
2015-09-07 16:56:32 +0300

    Add more printout

--- RimWorld_CompareTranslations.py
+++ RimWorld_CompareTranslations.py
@@ -21,6 +21,22 @@ def print_help_error():
     print("Enclose folder names in double quotes.")
 
 
+def print_progress(message, progress, total):
+    """Prints the number of files processed, total number of files and a percentage.
+
+    Replaces itself automatically and animates.
+    The format is progress/total percent%
+
+    :param progress: Number of files processed
+    :param total: Total number of files
+    """
+    # Calculate the percent, multiply with 1.0 to force floating point math
+    percent = 1.0 * progress / total
+    # Write the line, '\r' moves the write head back to the start for overwriting.
+    sys.stdout.write('\r{}: {}/{} {}%'.format(message, progress, total, round(percent * 100)))
+    sys.stdout.flush()
+
+
 def collect_tags(translationdir):
     """
 
@@ -110,31 +126,43 @@ elif not os.path.exists(secondDirPath):
 # dirpath is the full path to the current def directory, dirnames is a list of directories in the current directory
 # and filenames is a list of files
 
+print("Collecting tags...")
 firstList = collect_tags(firstDirPath)
+print("{} tags found in the first directory.".format(len(firstList)))
 secondList = collect_tags(secondDirPath)
+print("{} tags found in the second directory.".format(len(secondList)))
+print("")
 
+print("Comparing directories...")
 firstUnique = []
 secondUnique = []
 
-for element in firstList:
+for i, element in enumerate(firstList):
     if element not in secondList:
         firstUnique.append(element)
+    # print_progress('Comparing the first directory to the second one', i, len(firstList))
 
-for element in secondList:
+for i, element in enumerate(secondList):
     if element not in firstList:
         secondUnique.append(element)
+    # print_progress('Comparing the second directory to the first one', i, len(firstList))
 
 firstUnique = sort_tags_by_file(firstUnique)
+print("{} tags unique to the first directory.".format(len(firstUnique)))
 secondUnique = sort_tags_by_file(secondUnique)
+print("{} tags unique to the second directory.".format(len(secondUnique)))
+print("")
 
-print("Tags only in \"" + firstDirPath + "\":")
-for file, taglist in firstUnique:
-    print("    " + file)
-    for tag in taglist:
-        print("        " + tag)
-print("Tags only in \"" + secondDirPath + "\":")
-for file, taglist in secondUnique:
-    print("    " + file)
-    for tag in taglist:
-        print("        " + tag)
+if firstUnique:
+    print("Tags only in \"" + firstDirPath + "\":")
+    for file, taglist in firstUnique:
+        print("    " + file)
+        for tag in taglist:
+            print("        " + tag)
+if secondUnique:
+    print("Tags only in \"" + secondDirPath + "\":")
+    for file, taglist in secondUnique:
+        print("    " + file)
+        for tag in taglist:
+            print("        " + tag)
 print("")
