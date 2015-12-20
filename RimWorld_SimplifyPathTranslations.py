41d59cabdc467a26ad86daee6cd175d7c220ff4f
Saku Hakamaa <sakuukuli@hotmail.com>
2015-12-20 12:33:37 +0200

    add a python module with the most used functions

--- RimWorld_SimplifyPathTranslations.py
+++ RimWorld_SimplifyPathTranslations.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import xml.etree.ElementTree as ET
+import rwtutil
 
 __author__ = 'Sakuukuli'
 
@@ -19,29 +20,6 @@ def printhelperror():
     print("")
     print("Invalid number of arguments.")
     print("Enclose folder names in double quotes.")
-
-
-def format_path(path):
-    """Changes a path to a tag from 'rulesStrings[0]' to 'rulesStrings.0'.
-
-    :param path: A path as a string
-    :return: Formatted path
-    """
-    path = path.replace('[', '.')
-    path = path.replace(']', '')
-    return path
-
-
-def replace_escapechars(text):
-    """ Replaces HTML escape characters with their real equivalents.
-
-    :param text: Text to format
-    :return: Formatted text
-    """
-    escapechars = {'&quot;': '"', '&amp;': '&', '&lt;': '<', '&gt;': '>'}
-    for i, j in escapechars.items():
-        text = text.replace(i, j)
-    return text
 
 
 # Save the arguments
@@ -73,54 +51,6 @@ if not os.path.exists(translationDirPath):
     print("Directory is invalid.")
     sys.exit(2)
 
-# Go through all the folders one by one
-# dirpath is the full path to the current def directory, dirnames is a list of directories in the current directory
-# and filenames is a list of files
-for dirpath, dirnames, filenames in os.walk(translationDirPath):
-
-    # Only parse .xml files
-    for filename in [f for f in filenames if f.endswith('.xml')]:
-
-        # Parse the .xml file with ElementTree
-        defTree = ET.parse(os.path.join(dirpath, filename))
-        defRoot = defTree.getroot()
-
-        # Check if the file has path translations
-        if defRoot.find('rep'):
-            # Go through all the elements in the xml file
-            for index, repElement in enumerate(defRoot):
-                # Check if this element is a path translation
-                if repElement.tag == 'rep':
-                    # Store the information in the element
-                    path = repElement.find('path').text
-                    trans = repElement.find('trans').text
-                    whitespace = repElement.tail
-
-                    # Change the path to the new format
-                    path = format_path(path)
-
-                    # Remove the element in the old format
-                    defRoot.remove(repElement)
-
-                    # Insert an element in the new format in its place
-                    defRoot.insert(index, ET.Element(path))
-                    defRoot[index].text = trans
-                    defRoot[index].tail = whitespace
-
-            # Write the tree into a temporary file
-            tempfilename = filename + '.temp'
-            # Do not add a header, because it will be '<?xml version='1.0' encoding='utf-8'?>\n'
-            defTree.write(os.path.join(dirpath, tempfilename), encoding="utf-8", xml_declaration=False)
-
-            # Open the file for writing
-            defFile = open(os.path.join(dirpath, filename), 'w+')
-            tempFile = open(os.path.join(dirpath, tempfilename), 'r')
-            # Add header in the RimWorld way
-            defFile.write('<?xml version="1.0" encoding="utf-8" ?>\n')
-            defFile.write(replace_escapechars(tempFile.read()))
-            defFile.write('\n')
-
-            # Clean up
-            defFile.close()
-            tempFile.close()
-            os.remove(os.path.join(dirpath, tempfilename))
+print("Simplifying...", end=' ')
+rwtutil.simplify_path_translations(translationDirPath)
+print("OK")
--- RimWorld_TranslationUpgrader.py
+++ RimWorld_TranslationUpgrader.py
@@ -2,6 +2,7 @@ import os
 import sys
 import xml.etree.ElementTree as ET
 import shutil
+import rwtutil
 
 from datetime import date
 
@@ -23,78 +24,6 @@ def print_help_error():
     print("")
     print("Invalid number of arguments.")
     print("REMEMBER: Enclose folder names in double quotes.")
-
-
-def simplify_path_translations(translationdirpath):
-    """ Change the multiline path translations to the new simpler format
-
-    :param translationdirpath: Path to the translation to change
-    :return:
-    """
-    # Go through all the folders one by one
-    # dirpath is the full path to the current def directory, dirnames is a list of directories in the current directory
-    # and filenames is a list of files
-    for dirpath, dirnames, filenames in os.walk(translationdirpath):
-
-        # Only parse .xml files
-        for filename in [f for f in filenames if f.endswith('.xml')]:
-
-            # Parse the .xml file with ElementTree
-            deftree = ET.parse(os.path.join(dirpath, filename))
-            defroot = deftree.getroot()
-
-            # Check if the file has path translations
-            if defroot.find('rep'):
-                # Go through all the elements in the xml file
-                for index, repElement in enumerate(defroot):
-                    # Check if this element is a path translation
-                    if repElement.tag == 'rep':
-                        # Store the information in the element
-                        path = repElement.find('path').text
-                        trans = repElement.find('trans').text
-                        whitespace = repElement.tail
-
-                        # Change the path to the new format
-                        path = format_path(path)
-
-                        # Remove the element in the old format
-                        defroot.remove(repElement)
-
-                        # Insert an element in the new format in its place
-                        defroot.insert(index, ET.Element(path))
-                        defroot[index].text = trans
-                        defroot[index].tail = whitespace
-
-                # Write the tree into a temporary file
-                tempfilename = filename + '.temp'
-                # Do not add a header, because it will be '<?xml version='1.0' encoding='utf-8'?>\n'
-                deftree.write(os.path.join(dirpath, tempfilename), encoding="utf-8", xml_declaration=False)
-
-                # Open the file for writing
-                deffile = open(os.path.join(dirpath, filename), 'w+')
-                tempfile = open(os.path.join(dirpath, tempfilename), 'r')
-                # Add header in the RimWorld way
-                deffile.write('<?xml version="1.0" encoding="utf-8" ?>\n')
-                deffile.write(replace_escapechars(tempfile.read()))
-                deffile.write('\n')
-
-                # Clean up
-                deffile.close()
-                tempfile.close()
-                os.remove(os.path.join(dirpath, tempfilename))
-
-
-def format_path(path):
-    path = path.replace('[', '.')
-    path = path.replace(']', '')
-    return path
-
-
-def replace_escapechars(text):
-    escapechars = {'&quot;': '"', '&amp;': '&', '&lt;': '<', '&gt;': '>'}
-    for i, j in escapechars.items():
-        text = text.replace(i, j)
-    return text
 
 
 def print_progress(message, progress, total):
@@ -244,7 +173,7 @@ shutil.copytree(transPath, outPath)
 print("OK")
 
 # simplify old format multiline path translations
-simplify_path_translations(outPath)
+rwtutil.simplify_path_translations(outPath)
 
 # Go through all the folders one by one
 # transPath is the full path to the current def directory, dirnames is a list of directories in the current directory
